27423642655caf7d26401e54542abe346902d31b
JerryD <jerry.webdev@gmail.com>
2021-02-18 07:15:18 -0500

    Added the root component (app/src/GithubJobs.js) within the components directory

---
+++ src/components/app/src/GithubJobsApp.js
@@ -0,0 +1,22 @@
+import AppHeader from '../.../appheader/src/AppHeader.js';
+
+class GithubJobsApp extends HTMLEmbedElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML += `
+            <app-header></app-header>
+        `;
+    }
+}
+
+if (!window.customElements.get('github-jobs-app')) {
+    window.customElements.define('github-jobs-app', GithubJobsApp);
+}
