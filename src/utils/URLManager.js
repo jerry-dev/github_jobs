dfb1a0782e24ab44c1f61fc05dfffe54a29d3320
JerryD <jerry.webdev@gmail.com>
2021-03-17 10:50:05 -0400

    Commit Progress Made To The URLManager Helper Function

--- src/utils/URLManager.js
+++ src/utils/URLManager.js
@@ -1,8 +1,49 @@
 const URLManager = function(url) {
-    if (url === null || url === 'null' ) {
-        return '';
-    } else {
-        return url;
+
+    switch(url) {
+        case 'null':
+        case null:
+        case 'undefined':
+        case undefined:
+            return '';
+        default:
+            if (url.length < 8) {
+                return '';
+            } else {
+                let reformattedURL = url;
+
+                if (reformattedURL.includes('.com/')) {
+                    const regex = new RegExp('.com/');
+                    reformattedURL = reformattedURL.replace(regex, '.com');
+                }
+
+                if (reformattedURL.includes('.io/')) {
+                    const regex = new RegExp('.io/');
+                    reformattedURL = reformattedURL.replace(regex, '.io');
+                }
+
+                if (reformattedURL.includes('http')) {
+                    const regex = new RegExp('http:');
+                    reformattedURL = reformattedURL.replace(regex, '');
+                }
+
+                if (reformattedURL.includes('https:')) {
+                    const regex = new RegExp('https:');
+                    reformattedURL = reformattedURL.replace(regex, '');
+                }
+
+                if (reformattedURL.includes('//')) {
+                    const regex = new RegExp('//');
+                    reformattedURL = reformattedURL.replace(regex, '');
+                }
+
+                if (reformattedURL.includes('www.')) {
+                    const regex = new RegExp('www.');
+                    reformattedURL = reformattedURL.replace(regex, '');
+                }          
+                
+                return reformattedURL;
+            }
     }
 }
 
