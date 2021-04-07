9c6a49ca192cef9901ca0cb0c9c2586596c929c3
JerryD <jerry.webdev@gmail.com>
2021-04-07 11:45:43 -0400

    Restructure The "dist" Directory
    Changed the rollup output file format from "umd" to "iife"
    Retargeted rollup's output into a new dist directory called "app"
    Removed type="module" from the dist/index.html and updated the
    specified path within the dist/index.html file's script tag.
    Added seperate scripts within package.json's scripts to
    remove the dist/components and dist/utils directories
    Removed browserSyncDist script from the npm scripts due to issues

--- dist/src/utils/logoManager.js
+++
@@ -1,9 +0,0 @@
-const logoManager = function(url) {
-    if (url === 'null') {
-        return `<img rel="icon" class="icon" src="../../src/assets/icons/desktop/icons8-github-darktheme.svg">`
-    } else {
-        return `<img rel="icon" class="icon" src=${url}>`;
-    }
-}
-
-export default logoManager;
